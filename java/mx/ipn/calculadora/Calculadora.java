fac7859b7b264098c5cdf493d0afa47cd0035c12
Diego-torres-6IM7 <47031774+Diego-torres-6IM7@users.noreply.github.com>
2019-02-14 13:58:09 -0600

    Add files via upload

---
+++ java/mx/ipn/calculadora/Calculadora.java
@@ -0,0 +1,140 @@
+package mx.ipn.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class Calculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+
+
+
+
+
+    }
+    Double numero1,numero2,resultado;
+    String operador;
+
+    public void onClickButtonUno(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "1");
+    }
+    public void onClickButtonDos(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "2");
+    }
+    public void onClickButtonTres(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "3");
+    }
+    public void onClickButtonCuatro(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "4");
+    }
+    public void onClickButtonCinco(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "5");
+    }
+    public void onClickButtonSeis(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "6");
+    }
+    public void onClickButtonSiete(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "7");
+    }
+    public void onClickButtonOcho(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "8");
+    }
+    public void onClickButtonNueve(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "9");
+    }
+    public void onClickButtonCero(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + "0");
+    }
+
+    public void onClickButtonPunto(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText(tv.getText() + ".");
+    }
+
+    public void onClickOperacionCapturaNumero1(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        numero1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClickSuma(View miView)
+    {
+        operador="+";
+        onClickOperacionCapturaNumero1(miView);
+    }
+    public void onClickResta(View miView)
+    {
+        operador="-";
+        onClickOperacionCapturaNumero1(miView);
+    }
+    public void onClickMultiplicacion(View miView)
+    {
+        operador="*";
+        onClickOperacionCapturaNumero1(miView);
+    }
+    public void onClickDivision(View miView)
+    {
+        operador="/";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickIgual(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        numero2 = Double.parseDouble(tv.getText().toString());
+
+        try {
+
+            if (operador.equals("+")) {
+                resultado = numero1 + numero2;
+            } else if (operador.equals("-")) {
+                resultado = numero1 - numero2;
+            } else if (operador.equals("*")) {
+                resultado = numero1 * numero2;
+            } else if (operador.equals("/")) {
+                resultado = numero1 / numero2;
+            }
+            tv.setText(resultado.toString());
+        }catch(NumberFormatException nfe){
+            Toast.makeText(this,"Numero Incorrecto", LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClickLimpia(View miView)
+    {
+        numero1=0.0;
+        numero2=0.0;
+        TextView tv = (TextView) findViewById(R.id.textViewNumero) ;
+        tv.setText("");
+    }
+}
